f27dd69030107e221208dedcbd66dec14fa89557
DaisyLBell <daisybel@usc.edu>
2021-02-03 23:13:32 -0800

    the rest

---
+++ RCMP2.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function($){
+
+$('.pink-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_3_4.png');
+     }
+ });
+ 
+$('.grey-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_13.png');
+     }
+ });
+ 
+$('.lightblue-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_3_2.png');
+     }
+ });
+ 
+$('.lightpink-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_9.png');
+     }
+ });
+    
+    $('.hi-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_7.png');
+     }
+ });
+    
+    $('.hey-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_3_3.png');
+     }
+ });
+    
+    $('.ciao-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_6.png');
+     }
+ });
+    
+    $('.salut-button').on({
+     'click': function(){
+         $('#change-image').attr('src','Soup_5.png');
+     }
+ });
+    
+    
+});
